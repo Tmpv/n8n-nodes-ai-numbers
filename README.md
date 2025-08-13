528cd8e0a59ccb5b76e9d189885fd92e67857ab5
Toma Popov <t0ma.popov.90@gmail.com>
2025-08-13 13:19:13 +0300

    Initial realease for AI Numbers for n8n
    Only credentials definition, which can be used with the basic n8n HTTP
    node.

---
+++ README.md
@@ -0,0 +1,32 @@
+# n8n-nodes-currencytransfer
+
+This is an n8n community node. It lets authenticate with [AI Numbers](https://developer.ainumber.com/?shell#introduction) and use it in the HTTP node.
+
+[n8n](https://n8n.io/) is a [fair-code licensed](https://docs.n8n.io/reference/license/) workflow automation platform.
+
+[Installation](#installation)
+[Credentials](#credentials)
+[Compatibility](#compatibility)
+[Resources](#resources)
+
+## Installation
+
+Follow the [installation guide](https://docs.n8n.io/integrations/community-nodes/installation/) in the n8n community nodes documentation.
+
+## Credentials
+
+Create an account in [AI Numebrs](https://developer.ainumber.com)
+
+You will need to generate and API Key and API Secret in order to setup the
+authentication.
+
+
+## Compatibility
+
+The project was tested with `node` version `v20.15.1`
+
+## Resources
+
+* [n8n community nodes documentation](https://docs.n8n.io/integrations/community-nodes/)
+* [AI Numbers API](https://developer.ainumber.com/?shell#introduction)
+
---
+++ credentials/AINumbersApi.credentials.ts
@@ -0,0 +1,92 @@
+import type {
+  IAuthenticateGeneric,
+  ICredentialDataDecryptedObject,
+  ICredentialTestRequest,
+  ICredentialType,
+  IHttpRequestHelper,
+  INodeProperties,
+} from 'n8n-workflow';
+
+import crypto from 'crypto';
+
+export class AINumbersApi implements ICredentialType {
+  name = 'AINumbersApi';
+
+  displayName = 'AI Numbers credentials';
+  documentationUrl = 'https://developer.ainumber.com/?shell#authentication';
+
+  httpRequestNode = {
+    name: 'AI Numbers credentials',
+    docsUrl: 'https://developer.ainumber.com/?shell#introduction',
+    apiBaseUrl: 'https://api-v2.sendbee.io',
+  };
+
+  properties: INodeProperties[] = [
+    {
+      displayName: 'Auth Token',
+      name: 'authToken',
+      type: 'hidden',
+
+      typeOptions: {
+        expirable: true,
+      },
+      default: '',
+    },
+    {
+      displayName: 'API Key',
+      name: 'apiKey',
+      type: 'string',
+      typeOptions: { password: false },
+      required: true,
+      default: '',
+    },
+    {
+      displayName: 'API Secret',
+      name: 'apiSecret',
+      type: 'string',
+      typeOptions: { password: true },
+      required: true,
+      default: '',
+    },
+  ];
+
+  async preAuthentication(this: IHttpRequestHelper, credentials: ICredentialDataDecryptedObject) {
+    // 1. Get the current timestamp in seconds
+    const timestamp = Math.floor(Date.now() / 1000).toString();
+
+    // 2. Create HMAC SHA-256 signature (hex lowercase)
+    const apiSecretString = credentials.apiSecret as string;
+
+    const signature = crypto
+      .createHmac('sha256', apiSecretString)
+      .update(timestamp)
+      .digest('hex');
+
+    // 3. Concatenate timestamp and signature with a dot
+    const combined = `${timestamp}.${signature}`;
+
+    // 4. Convert the concatenated string into Base64
+    const base64 = Buffer.from(combined).toString('base64');
+
+    return { authToken: base64 };
+  }
+
+  authenticate: IAuthenticateGeneric = {
+    type: 'generic',
+    properties: {
+      headers: {
+        'X-Auth-Token': '={{$credentials.authToken}}',
+        'X-Api-Key': '={{$credentials.apiKey}}',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    },
+  };
+
+  test: ICredentialTestRequest = {
+    request: {
+      baseURL: 'https://api-v2.sendbee.io',
+      url: '/contacts',
+    },
+  };
+}
